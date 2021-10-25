4ff17607bcad414014e01dce98a9f8abda18b655
catupperman <catupperman@gmail.com>
2021-10-24 20:14:32 -0400

    trying to figure out routing for webpages

--- src/components/BevViberProject.js
+++ src/components/BevViberProject.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import BevViberImage from '../assets/images/bev-viber.jpeg';
-import {Link} from 'react-router-dom';
+// import React from 'react';
+// import BevViberImage from '../assets/images/bev-viber.jpeg';
+// import {Link} from 'react-router-dom';
 
-function BevViber(){
-    return (
-        <div class="card">
-        <Link to="https://guarded-meadow-10732.herokuapp.com/" target="_blank"> </Link>
-        <img src={BevViberImage}></img>
-        <p> An App for coffee and tea aficionados, which will generate a random coffee or tea beverage of your choice, after signing up or logging into the app. This app was built using HTML, CSS, JavaScript, node.js, Insomina for testing, GitHub for collaboration, Handlebars, and Heroku for deployment. <Link to="https://github.com/shobes572/The_Beverage_Viber " target="_blank">Here</Link> </p>
-    </div>  
-    )
-}
+// function BevViber(){
+//     return (
+//         <div class="card">
+//         {/* <Link to="https://guarded-meadow-10732.herokuapp.com/" target="_blank"> </Link> */}
+//         <img src={BevViberImage}></img>
+//         <p> An App for coffee and tea aficionados, which will generate a random coffee or tea beverage of your choice, after signing up or logging into the app. This app was built using HTML, CSS, JavaScript, node.js, Insomina for testing, GitHub for collaboration, Handlebars, and Heroku for deployment.</p>
+        
+//          {/* <Link to="https://github.com/shobes572/The_Beverage_Viber " target="_blank">Here</Link>  */}
+//     </div>  
+//     )
+// }
 
-export default BevViberImage();
+// export default BevViberImage();
