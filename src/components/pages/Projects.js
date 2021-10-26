868e56dad3880ba0e87fed40be23a1fb82430890
catupperman <catupperman@gmail.com>
2021-10-25 22:32:29 -0400

    project links

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -10,12 +10,12 @@ const imageStyle={
 function BevViber() {
     return (
         <div class="card">
-        <a href="https://guarded-meadow-10732.herokuapp.com/"></a>
+        <a href="https://guarded-meadow-10732.herokuapp.com/" target="_blank">Visit the App Here</a>
         <img style={imageStyle} src={BevViberImage}></img>
         <p style={{textAlign: "center"}}> An App for coffee and tea aficionados, which will generate a random coffee or tea beverage of your choice, after signing up or logging into the app. This app was built using HTML, CSS, JavaScript, node.js, Insomina for testing, GitHub for collaboration, Handlebars, and Heroku for deployment.</p>
         
         
-         {/* <Link to="https://github.com/shobes572/The_Beverage_Viber " target="_blank">Here</Link>  */ }
+         <a href="https://github.com/shobes572/The_Beverage_Viber " target="_blank">Check Out the GitHub link Here</a> 
     </div >  
     )
 }
