d99d41358f9b12fbcdbf2f28e6a1291d6a426525
catupperman <catupperman@gmail.com>
2021-10-25 21:33:35 -0400

    contact

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+    return(
+        <div id="contact-info">
+            <h3 style={{textAlign: "center"}}> Contact Me</h3>
+
+        </div>
+    )
+}
+
+export default Contact;
