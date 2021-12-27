2e21df879192b855bb39d6695c1f64e12bd52d04
catupperman <catupperman@gmail.com>
2021-12-27 14:25:55 -0500

    fixing about me

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -11,14 +11,12 @@ function AboutMe() {
         <div id="about-info">
             <h3 style={{textAlign: "center"}}>About Me</h3>
             <img style={imageStyle} src={aboutMeImage}></img>
-            <p style={{textAlign: "center"}}> Hello there! My name is Catherine, but everyone calls me 'Cat'. This is a showcase of my front end
-                development, thus far in my coding journey. It was baby steps that brought me into the coding world. I
-                had always had a flair for Art and Science. I love to paint, especially trees, and have recieved a BA in
-                Health Care Administration. So, coding?</p>
+            <p className= "para-one"> Hello there! My name is Catherine, better known as 'Cat'. This is a showcase of my full stack web
+                development, thus far in my coding journey.  I had always had a flair for Art and Science, and have a love for painting. Career wise, I have recieved a BA in Health Care Administration from the University of Arizona Global Campus in 2016, worked in finance as well as health care, and have a history of working closely with upper management . So, coding?</p>
 
                 <p style={{textAlign: "center"}}>Well, there was always soemthing pulling at me to create, and
                 build. During the pandemic, I fell in love with coding, but I wanted more. I decided to take the plunge
-                and attend the UPenn Full Stack Developer Boot Camp (pending certification November 2021) Coding is a
+                to attend the UPenn Full Stack Developer Boot Camp, and graduated November 13th, 2021.  Coding is a
                 beautiful blend of art, science, and frustration. Complex enough to make your head spin, but beautiful
                 enough to bring you back one keyboard stroke at a time. </p>
         </div>
