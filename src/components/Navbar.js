7a7eeed3304e8e1dc9e1c16ec747d3d5a2f74328
catupperman <catupperman@gmail.com>
2021-12-29 16:50:15 -0500

    NavBar button adjustments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,44 @@
 import React from 'react';
+import './styles/Navbar.css'
 
 function NavBar({ currentPage, handlePageChange}){
     return (
         <ul className="nav nav-tabs">
             <li className="nav-item">
-                <a
+                <button
                 href="#aboutme"
                 onClick={() => handlePageChange('AboutMe')}
                 className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
                 >
                     About
-                </a>
+                </button>
             </li>
             <li className="nav-item">
-                <a 
+                <button 
                 href="#contact"
                 onClick={() => handlePageChange('Contact')}
                 className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
                 >
                     Contact
-                </a>
+                </button>
             </li>
             <li className="nav-item">
-                <a
+                <button
                 href="#projects"
                 onClick={() => handlePageChange('Projects')}
                 className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
                 >
                     Projects
-                </a>
+                </button>
             </li>
             <li className="nav-item">
-                <a
+                <button
                 href="#resume"
                 onClick={() => handlePageChange('Resume')}
                 className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
                 >
                     Resume
-                </a>
+                </button>
             </li>
         </ul>
     )
